517862e062db0bbb6450c581da0c63c239aa353f
Zeb <zebhakansta@gmail.com>
2026-03-30 17:12:28 +0200

    mmmmMarabou

--- labs/1wordladders/bigbgig.py
+++ labs/1wordladders/bigbgig.py
@@ -1,7 +1,33 @@
-# The 'with' statement automatically closes the file for you
+import sys
 
-with open('example.txt', 'r', encoding='utf-8') as file:
-    for line in file:
-        # .strip() removes the newline character (\n) from the end
-        list.append(line.strip())
-        
+def solve():
+    with open('labs/1wordladders/data/sample/1.in', 'r', encoding='utf-8') as file:
+    # .strip() removes the \n at the end of each line
+        lines_list = [line.strip() for line in file]
+
+    words = []
+    paths = []
+
+    N, Q = lines_list[0].split()
+    N = int(N)
+    Q = int(Q)
+    for i in range(1, N):
+        words.append(lines_list[i])
+
+    for i in range(N+1, N + Q):
+        paths.append(lines_list[i])
+
+    connections = {}
+
+    for word in words:
+        for itWord in words:
+            if itWord != word and set(word[-4:].lower()).issubset(set(itWord.lower())):
+                if word not in connections:
+                    connections[word] = [itWord]  # Initialize the list if the key is new
+                    
+                connections[word].append(itWord)
+    print(connections)
+                
+
+
+solve()
