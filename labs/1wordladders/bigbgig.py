6f59273c431e177007a5da8e887ee8b4e619ff60
ellahagberg <143429227+ellahagberg@users.noreply.github.com>
2026-05-06 09:56:46 +0200

    lab1 working

--- labs/1wordladders/bigbgig.py
+++ labs/1wordladders/bigbgig.py
@@ -1,9 +1,40 @@
 import sys
-from collections import deque
+from collections import deque, Counter
+
+
+def can_go(word, other):
+    needed = Counter(word[-4:])
+    available = Counter(other)
+
+    for letter in needed:
+        if available[letter] < needed[letter]:
+            return False
+
+    return True
+
+
+def BFS(s: str, t: str, connections: dict):
+    visited = {s}
+    queue = deque([s])
+    distance = {s: 0}
+
+    while queue:
+        current = queue.popleft()
+
+        if current == t:
+            return distance[current]
+
+        for word in connections[current]:
+            if word not in visited:
+                visited.add(word)
+                queue.append(word)
+                distance[word] = distance[current] + 1
+
+    return "Impossible"
+
 
 def solve():
-    with open('labs/1wordladders/data/sample/1.in', 'r', encoding='utf-8') as file:
-    # .strip() removes the \n at the end of each line
+    with open('data/secret/3medium1.in', 'r', encoding='utf-8') as file:
         lines_list = [line.strip() for line in file]
 
     words = []
@@ -12,53 +43,26 @@ def solve():
     N, Q = lines_list[0].split()
     N = int(N)
     Q = int(Q)
-    for i in range(1, N):
+
+    for i in range(1, N + 1):
         words.append(lines_list[i])
 
-    for i in range(N+1, N + Q):
+    for i in range(N + 1, N + Q + 1):
         paths.append(lines_list[i])
 
     connections = {}
 
     for word in words:
+        connections[word] = []
+
+    for word in words:
         for itWord in words:
-            if itWord != word and set(word[-4:].lower()).issubset(set(itWord.lower())):
-                if word not in connections:
-                    connections[word] = [itWord]  # Initialize the list if the key is new
-                    
+            if itWord != word and can_go(word, itWord):
                 connections[word].append(itWord)
-    print(connections)
 
-#begin BFS
-def BFS(s: str, t: str, connections: dict):
-    visited = [s]
-    queue = deque([s])
-    pred = {}
-
-    while queue:
-
-        current = queue.popleft()
-
-        
-        
-        if current in connections.keys:
-            for word in connections.get(current):
-                if word not in visited:
-                    visited.append(word)
-                    queue.append(word)
-                    pred.update(word, current)
-                    if word == t:
-                        return {
-                            counter = 0
-                            while 
-
-                            ctr
-                        }
-
-
-
-    return "Impossible"
-
+    for path in paths:
+        start, end = path.split()
+        print(BFS(start, end, connections))
 
 
 solve()
