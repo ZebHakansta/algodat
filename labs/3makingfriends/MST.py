24a7cfac0cc944172e0374789b89323cedcf5de1
ellahagberg <143429227+ellahagberg@users.noreply.github.com>
2026-06-08 17:02:48 +0200

    Improvements and report

--- labs/3makingfriends/MST.py
+++ labs/3makingfriends/MST.py
@@ -1,51 +1,42 @@
 import sys
 
 
-def find(parent, nod):
-    # Folj kedjan av foraldrar tills vi hittar roten (nodens komponent-representant).
-    rot = nod
-    while parent[rot] != rot:
-        rot = parent[rot]
-    # Path compression: peka om alla noder pa vagen direkt mot roten,
-    # sa att nasta find blir snabbare.
-    while parent[nod] != rot:
-        parent[nod], nod = rot, parent[nod]
-    return rot
+def find(parent, node):
+    root = node
+    while parent[root] != root:
+        root = parent[root]
+    while parent[node] != root:
+        parent[node], node = root, parent[node]
+    return root
 
 
 def main():
-    # Las hela indata pa en gang och dela upp i en lista av heltal.
     data = sys.stdin.buffer.read().split()
     pos = 0
 
-    antal_personer = int(data[pos]); pos += 1
-    antal_kanter = int(data[pos]); pos += 1
+    amount_people = int(data[pos]); pos += 1
+    amount_edges = int(data[pos]); pos += 1
 
-    # Spara kanterna som (vikt, u, v) sa att sortering sker pa vikten.
-    kanter = []
-    for _ in range(antal_kanter):
+    edges = []
+    for _ in range(amount_edges):
         u = int(data[pos]); pos += 1
         v = int(data[pos]); pos += 1
-        vikt = int(data[pos]); pos += 1
-        kanter.append((vikt, u, v))
+        weight = int(data[pos]); pos += 1
+        edges.append((weight, u, v))
 
-    # Steg 1: sortera kanterna fran lattast till tyngst.
-    kanter.sort()
+    edges.sort()
+    parent = list(range(amount_people + 1))
 
-    # Varje nod borjar som sin egen komponent. Noderna ar 1..N.
-    parent = list(range(antal_personer + 1))
+    total_cost = 0
 
-    total_kostnad = 0
+    for weight, u, v in edges:
+        root_u = find(parent, u)
+        root_v = find(parent, v)
+        if root_u != root_v:
+            parent[root_u] = root_v  
+            total_cost += weight
 
-    # Steg 2: ta varje kant om den kopplar samman tva skilda komponenter.
-    for vikt, u, v in kanter:
-        rot_u = find(parent, u)
-        rot_v = find(parent, v)
-        if rot_u != rot_v:
-            parent[rot_u] = rot_v   # Sla ihop komponenterna.
-            total_kostnad += vikt
-
-    print(total_kostnad)
+    print(total_cost)
 
 
 main()
